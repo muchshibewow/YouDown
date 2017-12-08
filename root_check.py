2747a29d1a370a0eb65d9912493efecd889f6051
Pravin Pratap <pratappravin250@gmail.com>
2017-12-08 17:27:51 +0530

    Re-formatted code to (kind of) fit PEP-8 standards.

--- root_check.py
+++ root_check.py
@@ -2,18 +2,19 @@
 
 import os
 
+
 def Check(Path):
-	if os.name == 'nt': # For Windows users
-		try:
-			os.makedirs(Path+'\\Lolz')
-			os.rmdir(Path+'\\Lolz')
-			return True
-		except PermissionError as e:
-			return False
-	elif os.name=='posix': # For Linux users
-		try:
-			os.makedirs(Path+'/Lolz')
-			os.rmdir(Path+'/Lolz')
-			return True
-		except PermissionError as e:
-			return False
+    if os.name == 'nt':  # For Windows users
+        try:
+            os.makedirs(Path + '\\Lolz')
+            os.rmdir(Path + '\\Lolz')
+            return True
+        except PermissionError as e:
+            return False
+    elif os.name == 'posix':  # For Linux users
+        try:
+            os.makedirs(Path + '/Lolz')
+            os.rmdir(Path + '/Lolz')
+            return True
+        except PermissionError as e:
+            return False
