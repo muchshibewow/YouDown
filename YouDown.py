74369971f52acc7808418899a3b929d13b52c438
Pravin Pratap <pratappravin250@gmail.com>
2017-12-18 06:41:56 +0530

    Added comments.

--- YouDown.py
+++ YouDown.py
@@ -13,32 +13,45 @@ from PlayVid import PLAY
 print("Welcome to YouDown!")
 print("What would you like to download?")
 ans = int(input('1. Video\n2. Playlist\n:'))
+
+# Choices, choices..
 if ans == 1:
     # Downloading a single video only.
+
     URL = input("Enter the URL of the video : ")
     Path = input("Where do you want to save this file? : ")
     Video = Download(URL, Path)
+
+    # Video playback post-download.
     print("\nWould you like to play the video now? (y/n)")
     pl = input()
     if pl == 'y' or pl == 'Y':
         print("Playing")
         PLAY(Video)
     print("\n\nThank you for using YouDown!\n\n")
+
+
 elif ans == 2:
     # Downloading an entire playlist
+
     URL = input("Enter the URL of the playlist : ")
     Path = input("Where do you want to save these videos? : ")
     URLs = URLList(URL)
+
+    # Some random lolz.
     URLs.pop(2)
     URLs.pop(3)
     URLN = []
     for i in range(0, len(URLs), 2):
         URLN.append(URLs[i])
     URLs = URLN
+
+    # The actual downloading process.
     for Link in URLs[1:]:  # Don't ask.
         Download(Link, Path)
     print("\nPlaylist download complete!\n")
     print("\n\nThank you for using YouDown!\n\n")
-else:
+
+else: # You drunk, m8?
     print("There are only 2 options, mate. :3")
     exit()
