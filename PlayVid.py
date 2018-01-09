9ee47686d33c35b1cac2d11abf87e1865851279e
Pravin Pratap <pratappravin250@gmail.com>
2018-01-09 21:43:37 +0530

    Made this file stupidly simple.

--- PlayVid.py
+++ PlayVid.py
@@ -2,13 +2,10 @@
 # Adding support for playing videos post download.
 # Uses very stupid methods for using VLC for this.
 
-from subprocess import run
 import os
 
 
 def PLAY(Path):
-    # Determining the OS first.
-    if os.name == 'nt':  # Windows
-        os.startfile(Path)
-    elif os.name == 'posix':  # Linux
-        run('vlc ' + Path, shell=True) # Totally unrelated, listen to Run - AWOLNATION.
+    # Just one line to play the file.
+    # Equivalent to double-clicking the file.
+    os.startfile(Path)
